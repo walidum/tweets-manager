970a2c04233ee5a1439a4baeaa9a05d963cedd1b
boubechtoula <dw_boubechtoula@esi.dz>
2020-02-01 15:44:54 +0100

    corrctions

--- src/main/java/com/wbo/TwitterManager/service/TwitterService.java
+++ src/main/java/com/wbo/TwitterManager/service/TwitterService.java
@@ -5,8 +5,6 @@ import com.wbo.TwitterManager.model.entity.MyTweet;
 import com.wbo.TwitterManager.repo.TwitterRrepo;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -30,67 +28,25 @@ public class TwitterService {
     TwitterRrepo twitterRrepo;
 
     public List<TweetDto> searchHashtag(String hashtag) {
+        //chercher sur twiter
         List<Tweet> twitterList = searchTwiter(hashtag);
+        //chercher en local
         List<MyTweet> localLit = searchLocal(hashtag);
+        //alculer la différence entre les 2 premier
         List<MyTweet> diffList = diffLocalTwitter(twitterList, localLit);
+        //sauvgarder les nouveau éléments
         saveTweets(diffList);
+        //claculer le résultat à rtourner
         for (MyTweet myTweet : diffList) {
             localLit.add(myTweet);
         }
-        List<TweetDto> toReturn = localLit.stream().map(t -> new TweetDto(t)).collect(Collectors.toList());
-        return toReturn;
-    }
-
-    public MyTweet findTweetById(Long id) {
-        Optional< MyTweet> tweet = twitterRrepo.findById(id);
-        if (tweet.isPresent()) {
-            return tweet.get();
-        }
-        return null;
-    }
-
-    public MyTweet saveTweet(MyTweet tweet) {
-        MyTweet res = twitterRrepo.save(tweet);
-        return res;
-    }
-
-    public List<TweetDto> getTweets() {
-        List<MyTweet> list = twitterRrepo.findAll();
-        List<TweetDto> toReturn = list.stream().map(t -> new TweetDto(t)).collect(Collectors.toList());
-        return toReturn;
-    }
-
-    public void saveTweets(List<MyTweet> tweets) {
-        for (MyTweet tweet : tweets) {
-            saveTweet(tweet);
-        }
-    }
-
-    public List<MyTweet> diffLocalTwitter(List<Tweet> tweets, List<MyTweet> myTweets) {
-        List<Long> ids = myTweets.stream()
-                .map(t -> t.getId())
-                .collect(Collectors.toList());
-        List<Tweet> newList = new ArrayList<>();
-        for (Tweet t : tweets) {
-            if (!ids.contains(t.getId())) {
-                newList.add(t);
-            }
-        }
-        List<MyTweet> toReturn = newList.stream()
-                .map(t -> new MyTweet(t))
+        List<TweetDto> toReturn = localLit.stream()
+                .map(t -> new TweetDto(t))
                 .collect(Collectors.toList());
         return toReturn;
     }
 
-    public static <R> Predicate<R> not(Predicate<R> predicate) {
-        return predicate.negate();
-    }
-
-    public List<MyTweet> searchLocal(String hashtag) {
-        return twitterRrepo.findTweetsWithHashtag(hashtag);
-    }
-
-    public List<Tweet> searchTwiter(String hashtag) {
+    private List<Tweet> searchTwiter(String hashtag) {
         String consumerKey = env.getProperty("spring.social.twitter.consumerKey"); // The application's consumer key
         String consumerSecret = env.getProperty("spring.social.twitter.consumerSecret"); // The application's consumer secret
         String accessToken = env.getProperty("spring.social.twitter.app-id"); // The access token granted after OAuth authorization
@@ -102,15 +58,46 @@ public class TwitterService {
         return results.getTweets();
     }
 
-    public List<TweetDto> timelineTweets(String hashtag) {
-        String consumerKey = env.getProperty("spring.social.twitter.consumerKey"); // The application's consumer key
-        String consumerSecret = env.getProperty("spring.social.twitter.consumerSecret"); // The application's consumer secret
-        String accessToken = env.getProperty("spring.social.twitter.app-id"); // The access token granted after OAuth authorization
-        String accessTokenSecret = env.getProperty("spring.social.twitter.app-secret");// The access token secret granted after OAuth authorization
+    private List<MyTweet> diffLocalTwitter(List<Tweet> tweets, List<MyTweet> myTweets) {
+        if (myTweets != null && myTweets.size() > 0) {
+            List<Long> ids = myTweets.stream()
+                    .map(t -> t.getId())
+                    .collect(Collectors.toList());
+            List<Tweet> newList = new ArrayList<>();
+            for (Tweet t : tweets) {
+                if (!ids.contains(t.getId())) {
+                    newList.add(t);
+                }
+            }
+            List<MyTweet> toReturn = newList.stream()
+                    .map(t -> new MyTweet(t))
+                    .collect(Collectors.toList());
+            return toReturn;
+        }
+        return new ArrayList<>();
+    }
 
-        Twitter twitter = new TwitterTemplate(consumerKey, consumerSecret, accessToken, accessTokenSecret);
-        List<Tweet> tweets = twitter.timelineOperations().getUserTimeline(hashtag);
-        List<TweetDto> list = tweets.stream().map(t -> new TweetDto(t)).collect(Collectors.toList());
-        return list;
+    private MyTweet saveTweet(MyTweet tweet) {
+        MyTweet res = twitterRrepo.save(tweet);
+        return res;
+    }
+
+    //pour tester
+    public List<TweetDto> getTweets() {
+        List<MyTweet> list = twitterRrepo.findAll();
+        if (list != null && list.size() > 0) {
+            return list.stream().map(t -> new TweetDto(t)).collect(Collectors.toList());
+        }
+        return null;
+    }
+
+    private void saveTweets(List<MyTweet> tweets) {
+        for (MyTweet tweet : tweets) {
+            saveTweet(tweet);
+        }
+    }
+
+    private List<MyTweet> searchLocal(String hashtag) {
+        return twitterRrepo.findTweetsWithHashtag(hashtag);
     }
 }
