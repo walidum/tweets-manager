dedc41974b400327dc62d850784f57c27954cfa2
boubechtoula <dw_boubechtoula@esi.dz>
2020-02-15 17:30:00 +0100

    correction

--- src/main/java/com/wbo/TwitterManager/model/entity/MyTweet.java
+++ src/main/java/com/wbo/TwitterManager/model/entity/MyTweet.java
@@ -3,6 +3,8 @@ package com.wbo.TwitterManager.model.entity;
 import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import org.springframework.social.twitter.api.Tweet;
@@ -16,6 +18,7 @@ public class MyTweet implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String idStr;
