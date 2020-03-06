f42e6a29aca645fe43c5b15f67a33399c58949fc
boubechtoula <dw_boubechtoula@esi.dz>
2020-03-06 16:48:09 +0100

    CrossOrigin permit all

--- src/main/java/com/wbo/TwitterManager/controller/TwitterController.java
+++ src/main/java/com/wbo/TwitterManager/controller/TwitterController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author b.walid
  */
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class TwitterController {
 
     @Autowired
